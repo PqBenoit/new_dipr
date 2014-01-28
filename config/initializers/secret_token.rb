cde4b9b6fa624fbeab94997a0cc23bc1f2a77119
PqBenoit <benoit@monsieurdrive.com>
2014-01-28 14:43:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dipr2::Application.config.secret_token = '60ebda0110dfbe6587937302637989b26a3b7ada760b02130d34dff31a5a30fe8a35c819579f8327c79625b06ad2cc09ba30247e696a4717f3362aa70fa162ff'
